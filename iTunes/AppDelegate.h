7212997cf5291d0b803996e4614708bcdf783932
Nikhil Lele <nlele@smartzip.com>
2017-11-13 12:06:19 -0800

    Initial Commit

---
+++ iTunes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iTunes
+//
+//  Created by Nikhil Lele on 11/13/17.
+//  Copyright © 2017 Salesforce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iTunesTests/iTunesTests.m
@@ -0,0 +1,39 @@
+//
+//  iTunesTests.m
+//  iTunesTests
+//
+//  Created by Nikhil Lele on 11/13/17.
+//  Copyright © 2017 Salesforce. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iTunesTests : XCTestCase
+
+@end
+
+@implementation iTunesTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
