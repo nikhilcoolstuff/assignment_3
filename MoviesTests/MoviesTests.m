c844770769270d068c3a27cb10c977cbdd81bd68
Nikhil Lele <nlele@smartzip.com>
2017-11-14 11:11:00 -0800

    add app icons

--- MoviesTests/MoviesTests.m
+++ MoviesTests/MoviesTests.m
@@ -24,7 +24,7 @@
     [super tearDown];
 }
 
-- (void)testExample {
+- (void)testBasicSearch {
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
 }
